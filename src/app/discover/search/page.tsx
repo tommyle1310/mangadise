ca5d33f015b1f0546d7f03c358ebaa632990000d
tommyle1310 <tommyle1310@gmail.com>
2024-07-01 12:46:08 +0700

    refactor code in page.tsx(s)

--- src/app/discover/search/page.tsx
+++ src/app/discover/search/page.tsx
@@ -1,40 +1,11 @@
 'use client'
-import Category from '@/components/customComponents/Category'
-import useFetchSearch from '@/hooks/useFetchSearch';
-import usePagination from '@/hooks/usePagination';
-import { useRouter, useSearchParams } from 'next/navigation';
-import React from 'react'
+
+import DiscoverSearchPage from "@/components/customComponents/Discover/Search/DiscoverSearchPage"
 
 const page = () => {
-    const searchParams = useSearchParams()
-
-    const search = searchParams.get('query')
-
-    const { searchResultData, isLoading, searchResult, handleSearch, setQuery, query } = useFetchSearch({ initialQuery: search?.toString() });
-
-    const {
-        currentItems,
-        currentPage,
-        totalPages,
-        handlePageChange,
-        handleNextPage,
-        handlePreviousPage,
-    } = usePagination(searchResult, 5);
 
     return (
-        <div className=''>
-            <Category
-                currentPage={currentPage}
-                handleNext={handleNextPage}
-                handlePageChange={handlePageChange}
-                handlePrev={handlePreviousPage}
-                isLoading={isLoading}
-                list={currentItems}
-                title={{ normal: `${searchResult?.length} results found with keyword "${query}"` }}
-                totalPages={totalPages}
-                type='SEARCH_RESULTS'
-            />
-        </div>
+        <DiscoverSearchPage />
     )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import StoreProvider from "./Provider";
 import SessionWrapper from "@/components/SessionWrapper";
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
+import Footer from "@/components/customComponents/Footer/Footer";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -37,10 +38,7 @@ export default function RootLayout({
                 </div>
               </div>
               <Separator></Separator>
-              <div className="bg-black 2xl:ml-64 flex-1 p-4 lg:p-8 text-white tw-fc gap-3">
-                <p>© 2024 Mangadise. All rights reserved. Dive into the world of manga with Mangadise, your premier destination for reading manga online. Whether you're a seasoned manga enthusiast or new to the genre, Mangadise offers a vast collection of titles to explore. Join our community to discover, read, and discuss your favorite manga series. Enjoy seamless reading experiences, personalized recommendations, and stay updated with the latest releases. At Mangadise, we bring your manga dreams to life.</p>
-                <p>Huge thanks to the public API from: <Link className="text-primary font-bold" href='https://docs.otruyenapi.com/'>OTruyen API</Link></p>
-              </div>
+              <Footer />
             </div>
           </StoreProvider>
         </body>
